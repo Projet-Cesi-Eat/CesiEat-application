7c1b85d05217dd85c450cd5688e434c161edbb35
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-06-29 22:26:57 +0200

    Add routes

--- src/router.js
+++ src/router.js
@@ -1,69 +1,99 @@
-import Vue from "vue";
-import Router from "vue-router";
-import AppHeader from "./layout/AppHeader";
-import AppFooter from "./layout/AppFooter";
-import Components from "./views/Components.vue";
-import Landing from "./views/Landing.vue";
-import Login from "./views/Login.vue";
-import Register from "./views/Register.vue";
-import Profile from "./views/Profile.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import AppHeader from './layout/AppHeader';
+import AppFooter from './layout/AppFooter';
+import Components from './views/Components.vue';
+import Home from './views/Home.vue';
+import Landing from './views/Landing.vue';
+import Login from './views/Login.vue';
+import Register from './views/Register.vue';
+import Profile from './views/Profile.vue';
+import Restaurants from './views/Restaurants.vue';
+import RestaurantMenu from './views/RestaurantMenu.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  linkExactActiveClass: "active",
+  linkExactActiveClass: 'active',
   routes: [
     {
-      path: "/",
-      name: "components",
+      path: '/',
+      name: 'home',
+      components: {
+        header: AppHeader,
+        default: Home,
+        footer: AppFooter,
+      },
+    },
+    {
+      path: '/restaurants',
+      name: 'restaurants',
+      components: {
+        header: AppHeader,
+        default: Restaurants,
+        footer: AppFooter,
+      },
+    },
+    {
+      path: '/restaurant/:id',
+      name: 'restaurantmenu',
+      components: {
+        header: AppHeader,
+        default: RestaurantMenu,
+        footer: AppFooter,
+      },
+    },
+    {
+      path: '/components',
+      name: 'components',
       components: {
         header: AppHeader,
         default: Components,
-        footer: AppFooter
-      }
+        footer: AppFooter,
+      },
     },
     {
-      path: "/landing",
-      name: "landing",
+      path: '/landing',
+      name: 'landing',
       components: {
         header: AppHeader,
         default: Landing,
-        footer: AppFooter
-      }
+        footer: AppFooter,
+      },
     },
     {
-      path: "/login",
-      name: "login",
+      path: '/login',
+      name: 'login',
       components: {
         header: AppHeader,
         default: Login,
-        footer: AppFooter
-      }
+        footer: AppFooter,
+      },
     },
     {
-      path: "/register",
-      name: "register",
+      path: '/register',
+      name: 'register',
       components: {
         header: AppHeader,
         default: Register,
-        footer: AppFooter
-      }
+        footer: AppFooter,
+      },
     },
     {
-      path: "/profile",
-      name: "profile",
+      path: '/profile',
+      name: 'profile',
       components: {
         header: AppHeader,
         default: Profile,
-        footer: AppFooter
-      }
-    }
+        footer: AppFooter,
+      },
+    },
   ],
-  scrollBehavior: to => {
+  scrollBehavior: (to) => {
     if (to.hash) {
       return { selector: to.hash };
     } else {
       return { x: 0, y: 0 };
     }
-  }
+  },
 });
