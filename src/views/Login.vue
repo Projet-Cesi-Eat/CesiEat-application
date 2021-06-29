3686a9af601daf79e1d0b41653538d04e22ba2fa
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-06-29 22:27:29 +0200

    Update Login and Register form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,13 @@
 <template>
     <section class="section section-shaped section-lg my-0">
-        <div class="shape shape-style-1 bg-gradient-default">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
         <div class="container pt-lg-md">
             <div class="row justify-content-center">
                 <div class="col-lg-5">
-                    <card type="secondary" shadow
+                    <card type="default" shadow
                           header-classes="bg-white pb-5"
                           body-classes="px-lg-5 py-lg-5"
                           class="border-0">
                         <template>
-                            <div class="text-muted text-center mb-3">
-                                <small>Sign in with</small>
-                            </div>
-                            <div class="btn-wrapper text-center">
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/github.svg">
-                                    Github
-                                </base-button>
-
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/google.svg">
-                                    Google
-                                </base-button>
-                            </div>
-                        </template>
-                        <template>
-                            <div class="text-center text-muted mb-4">
-                                <small>Or sign in with credentials</small>
-                            </div>
                             <form role="form">
                                 <base-input alternative
                                             class="mb-3"
@@ -48,9 +19,6 @@
                                             placeholder="Password"
                                             addon-left-icon="ni ni-lock-circle-open">
                                 </base-input>
-                                <base-checkbox>
-                                    Remember me
-                                </base-checkbox>
                                 <div class="text-center">
                                     <base-button type="primary" class="my-4">Sign In</base-button>
                                 </div>
@@ -58,15 +26,10 @@
                         </template>
                     </card>
                     <div class="row mt-3">
-                        <div class="col-6">
-                            <a href="#" class="text-light">
-                                <small>Forgot password?</small>
-                            </a>
-                        </div>
-                        <div class="col-6 text-right">
-                            <a href="#" class="text-light">
+                        <div class="col text-center">
+                            <router-link class="link" to="/register">
                                 <small>Create new account</small>
-                            </a>
+                            </router-link>
                         </div>
                     </div>
                 </div>
@@ -78,4 +41,10 @@
 export default {};
 </script>
 <style>
+    .link{
+        color: #adb5bd
+    }
+    .link:hover{
+        color: #adb5bd
+    }
 </style>
