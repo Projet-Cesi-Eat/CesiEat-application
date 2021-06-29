a5b2841b5c82fa3cc3cde14d7082244203f935b9
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-06-29 22:27:56 +0200

    Create Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container ct-example-row">
+    <h1>CESI EAT</h1>
+  </div>
+</template>
+
+<script>
+import BaseNav from "@/components/BaseNav";
+
+export default {
+  components: {
+    BaseNav,
+  },
+}
+</script>
+
+<style>
+  
+</style>
