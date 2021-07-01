04403bdfc5ed2f1e5a95be27515d457d941869be
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-07-01 04:13:16 +0200

    Prettier format

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
-    <div id="app">
-        <router-view name="header"></router-view>
-        <main>
-            <fade-transition origin="center" mode="out-in" :duration="250">
-                <router-view/>
-            </fade-transition>
-        </main>
-        <router-view name="footer"></router-view>
-    </div>
+	<div id="app">
+		<router-view name="header"></router-view>
+		<main>
+			<fade-transition origin="center" mode="out-in" :duration="250">
+				<router-view />
+			</fade-transition>
+		</main>
+		<router-view name="footer"></router-view>
+	</div>
 </template>
 <script>
-import { FadeTransition } from "vue2-transitions";
+import { FadeTransition } from 'vue2-transitions';
 
 export default {
-  components: {
-    FadeTransition
-  }
+	components: {
+		FadeTransition
+	}
 };
 </script>
 <style>
-  #app{
-    min-height: 100vh;
-    position: relative;
-    margin: 0;
-    padding-bottom: 130px;
-    box-sizing: border-box;
-  }
+#app {
+	min-height: 100vh;
+	position: relative;
+	margin: 0;
+	padding-bottom: 130px;
+	box-sizing: border-box;
+}
 </style>
