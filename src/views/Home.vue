2236f2fe2e3cd09a695a0916796c126b770566d3
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-07-01 04:16:33 +0200

    Update home to use cookie on login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,47 @@
 <template>
-  <div class="container ct-example-row">
-    <h1>CESI EAT</h1>
-  </div>
+	<div>
+		<div class="container ct-example-row">
+			<img
+				class="banner__home"
+				:src="require('../assets/img/home-banner.jpg')"
+				alt="CesiEat Banner"
+			/>
+			<p v-if="user">{{ user }}</p>
+			<p v-else>Not logged</p>
+		</div>
+	</div>
 </template>
 
 <script>
-import BaseNav from "@/components/BaseNav";
+import BaseNav from '@/components/BaseNav';
+import Vue from 'vue';
+import Vuecookie from 'vue-cookie';
+
+Vue.use(Vuecookie);
 
 export default {
-  components: {
-    BaseNav,
-  },
-}
+	components: {
+		BaseNav
+	},
+	data: () => {
+		return {
+			user: null
+		};
+	},
+	mounted() {
+		if (this.$cookie.get('token')) {
+			this.user = localStorage.firstname;
+		}
+	}
+};
 </script>
 
 <style>
-  
+/* .banner {
+	max-height: 50vh;
+} */
+.banner__home {
+	max-height: 65vh;
+	width: 100%;
+}
 </style>
