0691b3b6ac0283c4f53069aeb8664d0a9272d479
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-07-01 04:16:09 +0200

    Update footer and header

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,49 +1,63 @@
 <template>
-    <footer class="has-cards">
-        <div class="container">
-            <hr>
-            <div class="row align-items-center justify-content-md-between">
-                <div class="col-md-6">
-                    <div class="copyright">
-                        &copy; {{year}}
-                        <a href="https://www.creative-tim.com" target="_blank" rel="noopener">Louis</a>, 
-                        <a href="https://www.binarcode.com" target="_blank" rel="noopener">Jailys</a> &
-                        <a href="https://www.binarcode.com" target="_blank" rel="noopener">Peter</a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <ul class="nav nav-footer justify-content-end">
-                        <li class="nav-item">
-                            <a target="_blank" rel="noopener" href="https://github.com/Projet-Cesi-Eat"
-                                class="btn btn-neutral btn-icon-only btn-github btn-round btn-lg" data-toggle="tooltip"
-                                data-original-title="Star on Github">
-                                <i class="fa fa-github"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+	<footer class="has-cards">
+		<div class="container">
+			<hr />
+			<div class="row align-items-center justify-content-md-between">
+				<div class="col-md-6">
+					<div class="copyright">
+						&copy; {{ year }} - Developped by
+						<a
+							href="https://github.com/louisbosqui"
+							target="_blank"
+							rel="noopener"
+							>Louis</a
+						>,
+						<a href="https://github.com/Jailys" target="_blank" rel="noopener"
+							>Jailys</a
+						>
+						&
+						<a
+							href="https://github.com/petergiromagny"
+							target="_blank"
+							rel="noopener"
+							>Peter</a
+						>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<a
+						target="_blank"
+						rel="noopener"
+						href="https://github.com/Projet-Cesi-Eat"
+						class="btn btn-neutral btn-icon-only btn-github btn-round btn-lg float-right"
+						data-toggle="tooltip"
+						data-original-title="Star on Github"
+					>
+						<i class="fa fa-github"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</footer>
 </template>
 <script>
 export default {
-  name: 'app-footer',
-  data() {
-    return {
-      year: new Date().getFullYear()
-    }
-  }
+	name: 'app-footer',
+	data() {
+		return {
+			year: new Date().getFullYear()
+		};
+	}
 };
 </script>
 <style>
- footer{ 
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 130px;
- }
- footer > hr {
-    margin-bottom: 0;
- }
+footer {
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+	height: 130px;
+}
+footer hr {
+	margin-bottom: 1.5rem;
+}
 </style>
--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,54 +1,113 @@
 <template>
-  <header class="header-global">
-    <base-nav class="navbar-main" transparent type="default" effect="dark" expand>
-        <div class="col-6 collapse-brand">
-          <router-link slot="brand" class="header__link header__link-primary mr-lg-5" to="/">
-            <span>Cesi Eat</span>
-          </router-link>
-          <router-link slot="brand" class="header__link mr-lg-5" to="/components">
-            <span>Components</span>
-          </router-link>
-          <router-link slot="brand" class="header__link mr-lg-5" to="/restaurants">
-            <span>Restaurants</span>
-          </router-link>
-        </div>
-        <div class="col-6 collapse-brand header__right">
-          <router-link slot="brand" class="navbar-brand mr-lg-2" to="/login">
-             <base-button type="neutral">Login</base-button>
-          </router-link>
-          <router-link slot="brand" class="navbar-brand" to="/register">
-             <base-button type="primary">Register</base-button>
-          </router-link>
-        </div>
-    </base-nav>
-  </header>
+	<header class="header-global">
+		<base-nav
+			class="navbar-main"
+			transparent
+			type="default"
+			effect="dark"
+			expand
+		>
+			<div class="col-6 collapse-brand">
+				<router-link
+					slot="brand"
+					class="header__link header__link-primary mr-lg-5"
+					to="/"
+				>
+					<span>Cesi Eat</span>
+				</router-link>
+				<router-link slot="brand" class="header__link mr-lg-5" to="/components">
+					<span>Components</span>
+				</router-link>
+				<router-link
+					slot="brand"
+					class="header__link mr-lg-5"
+					to="/restaurants"
+				>
+					<span>Restaurants</span>
+				</router-link>
+			</div>
+			<div v-if="token === null" class="col-6 collapse-brand header__right">
+				<router-link slot="brand" class="navbar-brand mr-lg-2" to="/login">
+					<base-button type="neutral">Login</base-button>
+				</router-link>
+				<router-link slot="brand" class="navbar-brand" to="/register">
+					<base-button type="primary">Register</base-button>
+				</router-link>
+				<p>{{ token }}</p>
+			</div>
+			<div v-else class="col-6 collapse-brand header__right">
+				<router-link slot="brand" class="header__link mr-lg-5" to="/components">
+					<span>{{ firstname }} {{ lastname }}</span>
+				</router-link>
+				<a class="navbar-brand" @click="logout">
+					<base-button type="neutral">Logout</base-button>
+				</a>
+			</div>
+		</base-nav>
+	</header>
 </template>
 <script>
-import BaseNav from "@/components/BaseNav";
+import BaseNav from '@/components/BaseNav';
+import Vue from 'vue';
+import Vuecookie from 'vue-cookie';
+
+Vue.use(Vuecookie);
 
 export default {
-  components: {
-    BaseNav
-  },
+	components: {
+		BaseNav
+	},
+	data() {
+		return {
+			token: this.$cookie.get('token') || null,
+			firstname: localStorage.getItem('firstname') || null,
+			lastname: localStorage.getItem('lastname') || null
+		};
+	},
+	methods: {
+		logout() {
+			this.$cookie.delete('token');
+
+			localStorage.removeItem('firstname');
+			localStorage.removeItem('lastname');
+			localStorage.removeItem('email');
+			localStorage.removeItem('siret');
+			localStorage.removeItem('numstreet');
+			localStorage.removeItem('street');
+			localStorage.removeItem('points');
+			localStorage.removeItem('sponsor');
+			localStorage.removeItem('sponsorised');
+			localStorage.removeItem('id_city');
+			localStorage.removeItem('id_img');
+			localStorage.removeItem('id_role');
+
+			if (this.$router.currentRoute.path != '/login') {
+				this.$router.push('/login');
+			}
+
+			location.reload();
+		}
+	}
 };
 </script>
 <style>
-  header{ 
-    height: 77px;
-  }
-  .header__link{
-    text-transform: none;
-    color: #f6f9fc 
-  }
-  .header__link:hover{
-    color: #f6f9fc 
-  }
-  .header__link-primary{
-    font-weight: bold;
-    font-size: 1.5em;
-  }
-  .header__right{
-    display: flex;
-    justify-content: flex-end;
-  }
+header {
+	height: 77px;
+}
+.header__link {
+	text-transform: none;
+	color: #f6f9fc;
+}
+.header__link:hover {
+	color: #f6f9fc;
+}
+.header__link-primary {
+	font-weight: bold;
+	font-size: 1.5em;
+}
+.header__right {
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+}
 </style>
